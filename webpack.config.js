37c6cf0da684ed2dbb85cd4af2c7b93f60741e6f
Umar Nazarov <1nazarovumar@gmail.com>
2022-01-19 18:14:55 +0500

    push

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
     output: {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: 'auto'
+        publicPath: '/'
     },
     resolve: {
         alias: {
