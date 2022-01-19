668a824ecd82c566ce4c52368b84f7c3c0654128
Umar Nazarov <1nazarovumar@gmail.com>
2022-01-19 11:53:38 +0500

    push

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Link, Route, Routes } from 'react-router-dom'
 
 function App() {
     const [s, set] = useState(0)
@@ -6,6 +7,14 @@ function App() {
         <div>
             <h1>{s}</h1>
             <button onClick={() => set(s + 1)}>+</button>
+            <Link to='/'>Home</Link>
+            <Link to='/topics'>Topics</Link>
+            <Link to='/about'>About</Link>
+            <Routes>
+                <Route path="/" element={<h1>Home</h1>} />
+                <Route path="/about" element={<h1>About</h1>} />
+                <Route path="/topics" element={<h1>Topics</h1>} />
+            </Routes>
         </div>
     )
 }
