78181ff69d5fe0335557e8b12ac9be7a7102e012
Umar Nazarov <1nazarovumar@gmail.com>
2022-01-19 18:21:37 +0500

    try

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import { HashRouter } from 'react-router-dom'
 
 ReactDOM.render(
-    <HashRouter basename="/">
+    <HashRouter basename={process.env.PUBLIC_URL}>
         <App />
     </HashRouter>,
     document.getElementById('root')
